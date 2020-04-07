9ca6cb6817aab369878b0aeb7f9c576e23ef1d4f
rhyme884 <rhyme884@gmail.com>
2020-04-07 19:42:17 +0900

    Client 모듈의 초기 체크 인

---
+++ Client/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ExecuteClient();
+        }
+
+        static void ExecuteClient()
+        {
+            try
+            {
+                string localTestIP = "192.168.0.36";
+                IPAddress ipAddr = IPAddress.Parse(localTestIP);
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 8888);
+
+                Socket sender = new Socket(ipAddr.AddressFamily,
+                            SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(localEndPoint);
+                    Console.WriteLine("Socket connected to -> {0} ", sender.RemoteEndPoint.ToString());
+
+                    byte[] messageSent = new byte[256];
+                    
+                    int byteSent = sender.Send(messageSent);
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+                catch (ArgumentNullException ane)
+                {
+                    Console.WriteLine("ArgumentNullException : {0} ", ane.ToString());
+                }
+                catch (SocketException se)
+                {
+                    Console.WriteLine("SocketException : {0} ", se.ToString());
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected Exception : {0} ", e.ToString());
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
